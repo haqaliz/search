959528206405fc00b8605918c123aa28434ea727
Ali Alizade Haqiqi <aliz@x-matik.com>
2022-01-04 03:10:00 +0330

    support regex in query
    Signed-off-by: Ali Alizade Haqiqi <aliz@x-matik.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::collections::HashMap;
+use regex::Regex;
 
 #[macro_use]
 extern crate colour;
@@ -47,23 +48,30 @@ pub fn run(params: Params) -> Result<(), Box<dyn Error>> {
         green_ln!("{}:", file);
 
         let contents = fs::read_to_string(file)?;
-        let results = search(&params.query, &contents);
+        let re = Regex::new(&params.query).unwrap();
+        let results = search(&re, &contents);
 
         for line in results {
-            let line_content = line.1.split(&params.query).collect::<Vec<&str>>();
+            let line_content = &re.split(&line.1).collect::<Vec<&str>>();
+            let line_captures = &re.find_iter(&line.1)
+                .map(|i| i.as_str())
+                .collect::<Vec<&str>>();
             let mut highlighted_before: bool = false;
 
             blue!("{}: ", line.0);
 
             for (i, item) in line_content.iter().enumerate() {
+                let ii = if i == 0 { 0 } else { i - 1 };
                 if item.is_empty() {
-                    red!("{}", &params.query);
+                    red!("{}", &line_captures[ii]);
                     highlighted_before = true;
                     continue;
                 }
 
-                if highlighted_before == false && i > 0 && i < line_content.len() {
-                    red!("{}", &params.query);
+                if highlighted_before == false
+                    && i > 0
+                    && i < line_content.len() {
+                    red!("{}", &line_captures[ii]);
                 }
 
                 print!("{}", item);
@@ -76,11 +84,11 @@ pub fn run(params: Params) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> HashMap<String, String> {
+pub fn search<'a>(re: &Regex, contents: &'a str) -> HashMap<String, String> {
     let mut lines = HashMap::new();
 
     for (i, line) in contents.lines().enumerate() {
-        if !line.contains(query) {
+        if !re.is_match(line) {
             continue;
         }
 
